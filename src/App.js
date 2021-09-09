2409c84c3c52cff46a12bcfdcfe89fcef0e622e4
ALVARO P <alv.gomez@outlook.com>
2021-09-09 16:31:20 +0200

    Display all listed API products

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import "./App.css";
+import { Switch, Route } from "react-router-dom";
+import Header from "./containers/Header";
+import ProductListing from "./containers/ProductListing";
+import ProductDetails from "./containers/ProductDetails";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>Hello World</h1>
+			<Header />
+			<Switch>
+				<Route path="/" exact component={ProductListing} />
+				<Route path="/product/:productId" exact component={ProductDetails} />
+				<Route>404 Not Found!</Route>
+			</Switch>
 		</div>
 	);
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
 import App from "./App";
+import store from "./redux/store";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<Router>
+			<Provider store={store}>
+				<App />
+			</Provider>
+		</Router>
 	</React.StrictMode>,
 	document.getElementById("root")
 );
